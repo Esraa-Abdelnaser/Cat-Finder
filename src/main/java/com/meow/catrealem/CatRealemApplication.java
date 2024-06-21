23185a8f6d940754274304c46cb3e024402d5122
Esraa Abdelnaser <esraaabdalnaser@gmail.com>
2024-06-21 03:02:56 +0300

    first commit

---
+++ src/main/java/com/meow/catrealem/CatRealemApplication.java
@@ -0,0 +1,13 @@
+package com.meow.catrealem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatRealemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatRealemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meow/catrealem/CatRealemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meow.catrealem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatRealemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
